790aa28f901c8084fd6530131785a2bbfb364646
Thaddee Tyl <thaddee.tyl@gmail.com>
2015-07-08 21:28:08 +0200

    use 1 insertion by default

--- generate.js
+++ generate.js
@@ -1,6 +1,7 @@
 var entropy = (+process.argv[2]) || 64;
+var nins = (+process.argv[3]) || 1;
 var passphrase = require('./main.js');
-passphrase(entropy, function(_, s, entr) {
+passphrase({entropy:entropy, insertions:nins}, function(_, s, entr) {
   if (_ != null) { console.error(_); return; }
   console.log('Entropy:', entr);
   console.log(s);
